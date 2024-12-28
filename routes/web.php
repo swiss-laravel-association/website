332c027e7edf837f51c4d49aaf4a3ed232bcf653
Stefan Zweifel <stefanzweifel@users.noreply.github.com>
2024-12-28 13:43:20 +0100

    Add Redirects to Socials and Forms (#1)

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
+
+
+// Redirects to forms
+Route::redirect('/feedback', 'https://forms.gle/aGtW8T8GrgZX4kAfA');
+Route::redirect('/rfp', 'https://forms.gle/M7gXenkv7F21ZYbMA');
+Route::redirect('/newsletter', 'https://sla.mailcoach.app/newsletter');
+
+
+// Social Media Redirects
+Route::redirect('/discord', 'https://discord.gg/Zpjd9ZkPcd');
+Route::redirect('/youtube', 'https://www.youtube.com/@swiss-laravel-association');
+Route::redirect('/github', 'https://github.com/swiss-laravel-association');
+Route::redirect('/bluesky', 'https://bsky.app/profile/laravel.swiss');
+Route::redirect('/linkedin', 'https://www.linkedin.com/company/swiss-laravel-association/');
+Route::redirect('/mastodon', 'https://phpc.social/@swiss_laravel_association');
+Route::redirect('/twitter', 'https://x.com/swisslaravel');
+Route::redirect('/x', 'https://x.com/swisslaravel');
