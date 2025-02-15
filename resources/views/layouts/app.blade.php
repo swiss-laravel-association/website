5d4f6b009f9ddb2415e28c7eadb7d9d083f74fe0
Stefan Zweifel <stefan@stefanzweifel.dev>
2025-02-15 15:55:24 +0100

    Add App Layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Swiss Laravel Association</title>
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+
+    <style>
+        [x-cloak] {
+            display: none;
+        }
+    </style>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+
+<body class="flex flex-col min-h-screen font-sans antialiased bg-gray-100">
+    {{ $slot }}
+
+    <x-footer/>
+
+    @livewireScripts
+</body>
+</html>
