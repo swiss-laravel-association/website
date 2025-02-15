96f4a186e98bb6c5e9daaf2041de501d56da3a9b
Stefan Zweifel <stefan@stefanzweifel.dev>
2025-02-15 16:24:15 +0100

    Name Redirects

--- routes/web.php
+++ routes/web.php
@@ -7,20 +7,20 @@ Route::get('/', function () {
 })->name('home');
 
 // Redirects to forms
-Route::redirect('/feedback', 'https://forms.gle/aGtW8T8GrgZX4kAfA');
-Route::redirect('/rfp', 'https://forms.gle/M7gXenkv7F21ZYbMA');
-Route::redirect('/newsletter', 'https://sla.mailcoach.app/newsletter');
-Route::redirect('/code-of-conduct', 'https://github.com/swiss-laravel-association/policies/blob/main/policies/CODE_OF_CONDUCT.md');
+Route::redirect('/feedback', 'https://forms.gle/aGtW8T8GrgZX4kAfA')->name('links.feedback');
+Route::redirect('/rfp', 'https://forms.gle/M7gXenkv7F21ZYbMA')->name('links.rfp');
+Route::redirect('/newsletter', 'https://sla.mailcoach.app/newsletter')->name('links.newsletter');
+Route::redirect('/code-of-conduct', 'https://github.com/swiss-laravel-association/policies/blob/main/policies/CODE_OF_CONDUCT.md')->name('links.code_of_conduct');
 
 // Social Media Redirects
-Route::redirect('/discord', 'https://discord.gg/Zpjd9ZkPcd');
-Route::redirect('/whatsapp', 'https://chat.whatsapp.com/J8l1kyqIwK209nrT89Ns2H');
-Route::redirect('/youtube', 'https://www.youtube.com/@swiss-laravel-association');
-Route::redirect('/github', 'https://github.com/swiss-laravel-association');
-Route::redirect('/bluesky', 'https://bsky.app/profile/laravel.swiss');
-Route::redirect('/linkedin', 'https://www.linkedin.com/company/swiss-laravel-association/');
-Route::redirect('/mastodon', 'https://phpc.social/@swiss_laravel_association');
-Route::redirect('/twitter', 'https://x.com/swisslaravel');
-Route::redirect('/x', 'https://x.com/swisslaravel');
+Route::redirect('/discord', 'https://discord.gg/Zpjd9ZkPcd')->name('links.discord');
+Route::redirect('/whatsapp', 'https://chat.whatsapp.com/J8l1kyqIwK209nrT89Ns2H')->name('links.whatsapp');
+Route::redirect('/youtube', 'https://www.youtube.com/@swiss-laravel-association')->name('links.youtube');
+Route::redirect('/github', 'https://github.com/swiss-laravel-association')->name('links.github');
+Route::redirect('/bluesky', 'https://bsky.app/profile/laravel.swiss')->name('links.bluesky');
+Route::redirect('/linkedin', 'https://www.linkedin.com/company/swiss-laravel-association/')->name('links.linkedin');
+Route::redirect('/mastodon', 'https://phpc.social/@swiss_laravel_association')->name('links.mastodon');
+Route::redirect('/twitter', 'https://x.com/swisslaravel')->name('links.twitter');
+Route::redirect('/x', 'https://x.com/swisslaravel')->name('links.x');
 
 Route::get('/meetups/calendar.ics', \App\Http\Controllers\MeetupEventsCalendarController::class)->name('meetups.calendar');
