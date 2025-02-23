545a892eb04160466d075f04111ffa7715598041
Stefan Zweifel <stefan@stefanzweifel.dev>
2025-02-23 15:59:16 +0100

    Hide Author in blog.index

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -29,13 +29,13 @@
                                     {{ $post->excerpt }}
                                 </p>
                             </div>
+
+                            @if (false)
                             <div class="relative mt-8 flex items-center gap-x-4">
-                                @if (false)
                                     {{-- TODO: Use Avatar of User here --}}
                                     <img
                                         src="https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                                         alt="" class="size-10 rounded-full bg-gray-50">
-                                @endif
                                 <div class="text-sm/6">
                                     <p class="font-semibold text-gray-900">
                                         @foreach($post->authors as $author)
@@ -44,6 +44,7 @@
                                     </p>
                                 </div>
                             </div>
+                            @endif
                         </article>
 
                     @endforeach
