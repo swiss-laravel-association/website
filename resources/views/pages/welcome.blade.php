f546c541f94e0e5074aaeafb1fe9a6f2326dec5d
Sandro Gehri <sandrogehri@gmail.com>
2025-03-26 14:05:33 +0100

    Hide past events on the start page

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -56,13 +56,13 @@
     </section>
 
     <x-content.section id="events"
-                       key-words="Events"
+                       key-words="Next Events"
                        title="Meet the Swiss Laravel community"
                        red
     >
         <p class="mt-6 text-balance text-xl/8 text-primary-200 dark:text-gray-400">
         <ul role="list" class="mx-auto mt-20 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-            @foreach(\App\Models\Event::query()->where('is_published', true)->orderBy('start_date')->get() as $event)
+            @foreach(\App\Models\Event::query()->where('is_published', true)->where('end_date', '>', now()->addHours(2))->orderBy('start_date')->get() as $event)
                 <li x-data="{ collapsed: true }" wire:key="{{ $event->id }}">
                     <div class="relative">
                         <img class="aspect-[3/2] w-full rounded-2xl object-cover"
