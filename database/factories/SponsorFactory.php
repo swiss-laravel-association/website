3f880a6254d173b43f910abadf2a457ad6fdef2e
Stefan Zweifel <stefan@stefanzweifel.dev>
2025-07-30 16:41:27 +0200

    Add SponsorFactory

--- database/factories/SponsorFactory.php
+++ database/factories/SponsorFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Enums\SponsorType;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,25 @@ class SponsorFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'type' => SponsorType::Founding,
+            'name' => $this->faker->company(),
+            'website' => 'https://example.com',
+            'background_color' => '#ff6600',
+            'order' => 0,
         ];
+    }
+
+    public function isFoundingSponsor(): Factory|SponsorFactory
+    {
+        return $this->state(fn (array $attributes): array => [
+            'type' => SponsorType::Founding,
+        ]);
+    }
+
+    public function isLocationSponsor(): Factory|SponsorFactory
+    {
+        return $this->state(fn (array $attributes): array => [
+            'type' => SponsorType::Founding,
+        ]);
     }
 }
