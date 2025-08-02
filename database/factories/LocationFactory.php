60ba1c045b320a6b13219499012d05947b947ece
Stefan Zweifel <stefan@stefanzweifel.dev>
2025-08-02 09:14:23 +0200

    Add Location Factory

---
+++ database/factories/LocationFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Location;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class LocationFactory extends Factory
+{
+    protected $model = Location::class;
+
+    public function definition(): array
+    {
+        $this->faker->addProvider(new \Wnx\FakerSwissLocations\Provider\Location($this->faker));
+
+        /** @var \Wnx\FakerSwissLocations\Location $location */
+        $location = $this->faker->location();
+
+        return [
+            'name' => $this->faker->company(),
+            'address' => $this->faker->address(),
+            'zip_code' => $location->postcode,
+            'city' => $location->city,
+            'description' => null,
+            'notes' => null,
+            'capacity' => $this->faker->randomElement([20, 30, 40, 50]),
+        ];
+    }
+}
