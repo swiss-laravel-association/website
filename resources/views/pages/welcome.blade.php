d16f80043eda31e0f003026ad055c5b452439eca
Stefan Zweifel <stefan@stefanzweifel.dev>
2026-05-25 12:24:22 +0200

    Don't collapse event description

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -62,7 +62,7 @@
         <p class="mt-6 text-balance text-xl/8 text-primary-200 dark:text-gray-400">
         <ul role="list" class="mx-auto mt-20 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3">
             @foreach(\App\Models\Event::query()->where('is_published', true)->where('end_date', '>', now()->addHours(2))->orderBy('start_date')->get() as $event)
-                <li x-data="{ collapsed: true }" wire:key="{{ $event->id }}">
+                <li x-data="{ collapsed: false }" wire:key="{{ $event->id }}">
                     <div class="relative">
                         <img class="aspect-[3/2] w-full rounded-2xl object-cover"
                              src="{{ \Illuminate\Support\Facades\Vite::asset('resources/images/meetup/laravel-letters.webp') }}"
