9af4f4c1decc641122b82aaa394355f5be8563c2
Stefan Zweifel <stefan@stefanzweifel.dev>
2026-06-21 18:04:18 +0200

    Update Blog Design

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -1,33 +1,29 @@
 <x-app-layout>
-    <div class="bg-white py-24 sm:py-32">
-        <div class="mx-auto max-w-7xl px-6 lg:px-8">
 
-            <div class="mx-auto max-w-2xl">
-                <x-breadcrumbs :items="$breadcrumbs" class="mb-8" />
+    <x-sla-ui.container class="max-w-3xl">
+        <x-breadcrumbs :items="$breadcrumbs" class="mb-8"/>
 
-                <h1 class="text-pretty text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl mb-4">
-                    {{ $post->title }}
-                </h1>
+        <h1 class="text-pretty text-4xl font-semibold tracking-tight text-mauve-200 sm:text-5xl mb-4">
+            {{ $post->title }}
+        </h1>
 
-                <footer class="text-sm text-gray-700 font-semibold mb-4">
-                    <time
-                        class="dt-published"
-                        title="{{ $post->published_at->format('Y-m-d') }}"
-                        datetime="{{ $post->published_at->format('Y-m-d') }}"
-                    >
-                        {{ $post->published_at->format('M d, Y') }}
-                    </time>
+        <footer class="text-sm text-mauve-400 font-semibold mb-4">
+            <time
+                class="dt-published"
+                title="{{ $post->published_at->format('Y-m-d') }}"
+                datetime="{{ $post->published_at->format('Y-m-d') }}"
+            >
+                {{ $post->published_at->format('M d, Y') }}
+            </time>
 
-                    &bull;
-                    {{ $post->getReadingTimeInMinutes() }} min  read
-                </footer>
+            &bull;
+            {{ $post->getReadingTimeInMinutes() }} min read
+        </footer>
 
 
-                <div class="prose prose-lg">
-                    {!! $post->parsed_content !!}
-                </div>
-            </div>
-
+        <div class="prose prose-lg prose-invert">
+            {!! $post->parsed_content !!}
         </div>
-    </div>
+
+    </x-sla-ui.container>
 </x-app-layout>
