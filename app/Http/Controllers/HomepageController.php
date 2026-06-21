b1efa8285c7206becbfc9c0d50f599144a687b74
Stefan Zweifel <stefan@stefanzweifel.dev>
2026-06-21 13:26:00 +0200

    Pass Sponsors and Events to Homepage

--- app/Http/Controllers/HomepageController.php
+++ app/Http/Controllers/HomepageController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Event;
+use App\Models\Sponsor;
 use Illuminate\Contracts\View\View;
 
 class HomepageController extends Controller
@@ -9,7 +11,22 @@ class HomepageController extends Controller
     public function __invoke(): View
     {
         return view('pages.homepage', [
-            //
+            'nextEvent' => Event::query()
+                ->with(['location'])
+                ->where('start_date', '>', now())
+                ->orderBy('start_date')
+                ->first(),
+            'pastEvents' => Event::query()
+                ->with(['location'])
+                ->where('start_date', '<', now())
+                ->orderBy('start_date', 'desc')
+                ->get(),
+            'sponsors' => Sponsor::query()
+                ->with(['media'])
+                ->founding()
+                ->limit(8)
+                ->inRandomOrder()
+                ->get(),
         ]);
     }
 }
