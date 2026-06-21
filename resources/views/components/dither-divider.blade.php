44505cbae2306d30f83cf07e6f6ad00174221393
Stefan Zweifel <stefan@stefanzweifel.dev>
2026-06-21 16:49:47 +0200

    Update sponsoring

--- resources/views/components/dither-divider.blade.php
+++ resources/views/components/dither-divider.blade.php
@@ -2,12 +2,18 @@
     'canvasId',
     'height' => 28,
     'speed' => 0.01,
+    'fullBleed' => false,
 ])
 
-{{-- Full-bleed animated Bayer-dithered orange stripe. The animation is initialised in resources/js/ascii.js. --}}
+{{-- Animated Bayer-dithered orange stripe. The animation is initialised in resources/js/ascii.js. --}}
+{{-- When `fullBleed` is true the canvas breaks out of its parent and spans the full viewport width. --}}
 <canvas
     id="{{ $canvasId }}"
-    {{ $attributes->class(['block w-full']) }}
+    {{ $attributes->class([
+        'block',
+        'w-full' => ! $fullBleed,
+        'w-screen max-w-none relative left-1/2 -translate-x-1/2' => $fullBleed,
+    ]) }}
     style="height: {{ $height }}px;"
     data-dither-divider
     data-dither-height="{{ $height }}"
