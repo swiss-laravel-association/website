55f8d80e0d0a8343af7c46ac982913f40cf45e7e
Stefan Zweifel <stefan@stefanzweifel.dev>
2026-06-27 13:40:58 +0200

    Update Favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,9 +11,9 @@
     <link rel="me" href="https://x.com/swisslaravel">
     <link rel="me" href="https://github.com/swiss-laravel-association">
 
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=2026-06-27">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=2026-06-27">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=2026-06-27">
     <link rel="manifest" href="/site.webmanifest">
 
     <style>
